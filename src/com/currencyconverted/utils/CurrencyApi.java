97612509dac7de43292c1c765e65c2275595245f
Sojo506 <fsojodev@gmail.com>
2024-10-05 11:11:46 -0600

    added CurrencyApi Record to convert from json

---
+++ src/com/currencyconverted/utils/CurrencyApi.java
@@ -0,0 +1,6 @@
+package com.currencyconverted.utils;
+
+public record CurrencyApi(String result,
+                          String conversion_rate,
+                          String conversion_result) {
+}
