633bfcbbf70337d371a9293760876ee76ea4cb32
Sojo506 <fsojodev@gmail.com>
2024-10-06 01:26:36 -0600

    added logic to the cases

--- src/com/currencyconverted/ui/Menu.java
+++ src/com/currencyconverted/ui/Menu.java
@@ -1,5 +1,7 @@
 package com.currencyconverted.ui;
 
+import com.currencyconverted.utils.ExchangeConverter;
+
 import java.util.Map;
 import java.util.Scanner;
 
@@ -7,6 +9,10 @@ public class Menu {
     public static void start(Map<String, Double> rates) {
         Scanner scanner = new Scanner(System.in);
         int option;
+        int amount;
+        double convertedCurrency;
+        String fromCurrency;
+        String toCurrency;
 
         do {
             System.out.println("=============================================");
@@ -28,21 +34,57 @@ public class Menu {
             switch (option) {
                 case 1:
                     // Call conversion function from USD to ARS
+                    fromCurrency = "USD";
+                    toCurrency = "ARS";
+
+                    System.out.print("Enter the value you want to convert: ");
+                    amount = scanner.nextInt();
+                    convertedCurrency = ExchangeConverter.convertCurrency(rates, fromCurrency, toCurrency, amount);
                     break;
                 case 2:
                     // Call conversion function from ARS to USD
+                    fromCurrency = "ARS";
+                    toCurrency = "USD";
+
+                    System.out.print("Enter the value you want to convert: ");
+                    amount = scanner.nextInt();
+                    convertedCurrency = ExchangeConverter.convertCurrency(rates, fromCurrency, toCurrency, amount);
                     break;
                 case 3:
                     // Call conversion function from USD to BRL
+                    fromCurrency = "USD";
+                    toCurrency = "BRL";
+
+                    System.out.print("Enter the value you want to convert: ");
+                    amount = scanner.nextInt();
+                    convertedCurrency = ExchangeConverter.convertCurrency(rates, fromCurrency, toCurrency, amount);
                     break;
                 case 4:
                     // Call conversion function from BRL to USD
+                    fromCurrency = "BRL";
+                    toCurrency = "USD";
+
+                    System.out.print("Enter the value you want to convert: ");
+                    amount = scanner.nextInt();
+                    convertedCurrency = ExchangeConverter.convertCurrency(rates, fromCurrency, toCurrency, amount);
                     break;
                 case 5:
                     // Call conversion function from USD to COP
+                    fromCurrency = "USD";
+                    toCurrency = "COP";
+
+                    System.out.print("Enter the value you want to convert: ");
+                    amount = scanner.nextInt();
+                    convertedCurrency = ExchangeConverter.convertCurrency(rates, fromCurrency, toCurrency, amount);
                     break;
                 case 6:
                     // Call conversion function from COP to USD
+                    fromCurrency = "COP";
+                    toCurrency = "USD";
+
+                    System.out.print("Enter the value you want to convert: ");
+                    amount = scanner.nextInt();
+                    convertedCurrency = ExchangeConverter.convertCurrency(rates, fromCurrency, toCurrency, amount);
                     break;
                 case 7:
                     System.out.println("Exiting the converter. Goodbye!");
