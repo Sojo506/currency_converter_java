4f561a2842a4ab6a2abbbb96ba9c9d2d33aa7355
Sojo506 <fsojodev@gmail.com>
2024-10-05 23:52:35 -0600

    tested api that gets all currencies and then are filtered

--- src/com/currencyconverted/Main.java
+++ src/com/currencyconverted/Main.java
@@ -4,15 +4,52 @@ import com.currencyconverted.api.ExchangeRateClient;
 import com.currencyconverted.utils.CurrencyApi;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/*
+ARS - Peso argentino
+
+BOB - Boliviano boliviano
+
+BRL - Real brasileño
+
+CLP - Peso chileno
+
+COP - Peso colombiano
+
+USD - Dólar estadounidense
+*/
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         CurrencyApi currencyApi = ExchangeRateClient
-                .getConvertedAmount(
-                        "CRC",
+                .getDynamicExchange(
                         "USD",
+                        "CRC",
                         5000);
-
         System.out.println("currencyApi = " + currencyApi);
+
+        Map<String, Double> rates = ExchangeRateClient.getCurrencyExchange().conversion_rates();
+
+        // Definir la lista de monedas que queremos filtrar
+        List<String> filteredCurrencies = Arrays.asList("ARS", "BOB", "BRL", "CLP", "COP", "USD");
+
+        // Crear un nuevo Map para almacenar solo las conversiones filtradas
+        Map<String, Double> filteredRates = new HashMap<>();
+
+        // Filtrar las tasas de cambio
+        for (String currency : filteredCurrencies) {
+            if (rates.containsKey(currency)) {
+                filteredRates.put(currency, rates.get(currency));
+            }
+        }
+
+        filteredRates.forEach((currency, rate) -> {
+            System.out.println(currency + ": " + rate);
+        });
+
     }
 }
