12d256d79c8ba438851a36f7d03f03785d887034
Sojo506 <fsojodev@gmail.com>
2024-10-06 01:27:45 -0600

    tested Menu and ExchangeConverter classes

--- src/com/currencyconverted/Main.java
+++ src/com/currencyconverted/Main.java
@@ -1,12 +1,11 @@
 package com.currencyconverted;
 
 import com.currencyconverted.api.ExchangeRateClient;
+import com.currencyconverted.ui.Menu;
 import com.currencyconverted.utils.CurrencyApi;
+import com.currencyconverted.utils.CurrencyFilteredUtils;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 /*
@@ -21,35 +20,30 @@ CLP - Peso chileno
 COP - Peso colombiano
 
 USD - Dólar estadounidense
+
+Dolar =>> Peso Argentino
+Peso Argentino =>> Dolar
+Dolar =>> Real Brasileño
+Real Brasileño ==> Dolar
+Dolar =>> Peso Colombiano
+Peso Colombiano =>> Dolar
 */
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
-        CurrencyApi currencyApi = ExchangeRateClient
+        /*CurrencyApi currencyApi = ExchangeRateClient
                 .getDynamicExchange(
                         "USD",
                         "CRC",
                         5000);
-        System.out.println("currencyApi = " + currencyApi);
+        System.out.println("currencyApi = " + currencyApi);*/
 
         Map<String, Double> rates = ExchangeRateClient.getCurrencyExchange().conversion_rates();
 
-        // Definir la lista de monedas que queremos filtrar
-        List<String> filteredCurrencies = Arrays.asList("ARS", "BOB", "BRL", "CLP", "COP", "USD");
+        Map<String, Double> filteredRates = CurrencyFilteredUtils.getFilteredRates(rates);
+        System.out.println("filteredRates = " + filteredRates);
 
-        // Crear un nuevo Map para almacenar solo las conversiones filtradas
-        Map<String, Double> filteredRates = new HashMap<>();
-
-        // Filtrar las tasas de cambio
-        for (String currency : filteredCurrencies) {
-            if (rates.containsKey(currency)) {
-                filteredRates.put(currency, rates.get(currency));
-            }
-        }
-
-        filteredRates.forEach((currency, rate) -> {
-            System.out.println(currency + ": " + rate);
-        });
+        Menu.start(filteredRates);
 
     }
 }
