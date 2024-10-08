800056820b7302fd834c210386936b6b0d526d44
Sojo506 <fsojodev@gmail.com>
2024-10-07 22:26:46 -0600

    removed comments

--- src/com/currencyconverted/Main.java
+++ src/com/currencyconverted/Main.java
@@ -6,27 +6,6 @@ import com.currencyconverted.ui.Menu;
 import java.io.IOException;
 import java.util.Map;
 
-/*
-ARS - Peso argentino
-
-BOB - Boliviano boliviano
-
-BRL - Real brasileño
-
-CLP - Peso chileno
-
-COP - Peso colombiano
-
-USD - Dólar estadounidense
-
-Dolar =>> Peso Argentino
-Peso Argentino =>> Dolar
-Dolar =>> Real Brasileño
-Real Brasileño ==> Dolar
-Dolar =>> Peso Colombiano
-Peso Colombiano =>> Dolar
-*/
-
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
         Map<String, Double> rates = ExchangeRateClient.getCurrencyExchange().conversion_rates();
